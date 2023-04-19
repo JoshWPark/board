11c2fde1c22e44d095d05c614ff3e5f5ef84985a
seokscirbe <seokscribe1970@gmail.com>
2023-04-20 01:11:59 +0900

    회원가입 검증 구현

--- src/main/java/com/sparta/board/dto/AuthRequestDto.java
+++ src/main/java/com/sparta/board/dto/AuthRequestDto.java
@@ -1,11 +1,13 @@
 package com.sparta.board.dto;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
 @Getter
 public class AuthRequestDto {
+    @Pattern(regexp = "^[a-z0-9]{4,10}$",message = "아이디는 4자이상 10자이하")
     private String username;
+    @Pattern(regexp = "^[a-zA-Z0-9]{8,15}$",message = "비밀번호 8자이상 15자이하")
     private String password;
 }
